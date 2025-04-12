c76d35fd30c9af71debccd841d12063b36f5833a
Genki Otake <231205037@ccmailg.meijo-u.ac.jp>
2025-04-12 16:41:56 +0900

    テスト

---
+++ backend/db/config.php
@@ -0,0 +1,6 @@
+<?php
+    define(DB_HOST_NAME, '85.131.207.165');
+    define(DB_NAME, 'miso079274_pachiyame01');
+    define(DB_USER_NAME, 'miso079374_ad');
+    define(DB_PASSWORD, 'pachinkostop');
+?>
